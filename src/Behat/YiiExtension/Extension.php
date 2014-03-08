083372888ad215f51abe6b1f0a8a58b64ff6e064
Patrick Dreyer <Patrick@Dreyer.name>
2014-03-08 01:20:59 +0100

    CHG: Make wunit a boolean value

--- src/Behat/YiiExtension/Extension.php
+++ src/Behat/YiiExtension/Extension.php
@@ -57,7 +57,7 @@ class Extension extends BaseExtension
         }
         $container->setParameter('behat.yii_extension.config_script', $config['config_script']);
 
-        if (isset($config['wunit'])) {
+        if ($config['wunit']) {
             if (!class_exists('Behat\\Mink\\Driver\\WUnitDriver')) {
                 throw new \RuntimeException(
                     'Install WUnitDriver in order to activate wunit session.'
@@ -83,9 +83,8 @@ class Extension extends BaseExtension
                 scalarNode('config_script')->
                     isRequired()->
                 end()->
-                arrayNode('wunit')->
-                    children()->
-                    end()->
+                booleanNode('wunit')->
+                  defaultFalse()->
                 end()->
             end()
         ;
